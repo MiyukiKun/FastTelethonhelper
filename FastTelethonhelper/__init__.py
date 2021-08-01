473bdbd8cf9fa5fba35d4117949117c2835064d5
Wolfy <63961377+Wolfy024@users.noreply.github.com>
2021-08-02 01:47:56 +0530

    Update __init__.py

--- FastTelethonhelper/__init__.py
+++ FastTelethonhelper/__init__.py
@@ -2,10 +2,22 @@ import sys
 import pathlib
 import time
 import datetime as dt
+import os
+import telethon
 sys.path.insert(0, f"{pathlib.Path(__file__).parent.resolve()}\\")
 
 from FastTelethon import upload_file, download_file
+######################### CUSTOM ERRORS ############################
+class Nothing_Defined_Error(Exception):
+    def __init__(self,message="Define either a reply message(reply arg) or a message to edit(edit_message arg).\n For more info, Visit https://pypi.org/project/FastTelethonhelper/"):
+        self.message=message
+        super().__init__(self.message)
 
+class Both_Defined_Error(Exception):
+    def __init__(self,message="Define one of the two arguments(reply & edit_message). Defining both isn't allowed.\n For more info, Visit https://pypi.org/project/FastTelethonhelper/"):
+        self.message=message
+        super().__init__(self.message)
+####################################################################
 class Timer:
     def __init__(self, time_between=5):
         self.start_time = time.time()
@@ -24,22 +36,40 @@ def human_readable_size(size, decimal_places=2):
         size /= 1024.0
     return f"{size:.{decimal_places}f} {unit}"
 
-async def download_with_progressbar(client, reply, msg):
+async def download_with_progressbar(client, message, path, edit_message=None, end_message=None, reply=None):
     timer = Timer()
-
+    if reply==None and edit_message==None:
+        raise  Nothing_Defined_Error
+    
+    if reply!=None and edit_message!=None:
+        raise Both_Defined_Error
+    ################## Checking if the argument given is valid or not ################
+    if reply==None:
+        reply=edit_message
+    try:
+        _=reply.id
+    except:
+        raise telethon.errors.rpcbaseerrors.NotFoundError
+    ####################################################################################
     async def progress_bar(downloaded_bytes, total_bytes):
         if timer.can_send():
             data = progress_bar_str(downloaded_bytes, total_bytes)
             await reply.edit(f"Downloading...\n{data}")
-
-    file = msg.document
-    filename = msg.file.name
-    dir = f"downloads/"
+    file = message.document
+    filename = message.file.name
+    dir = f"{path}"
+    try:
+        if os.path.exists(path):
+            pass
+        else:
+            os.makedirs(path)
+    except SyntaxError:
+        raise SyntaxError   
     if not filename:
         filename = (
             "video_" + dt.now().isoformat("_", "seconds") + ".mp4"
                     )
-    download_location = dir + filename
+    download_location = os.path.join(dir,filename)
     with open(download_location, "wb") as f:
         await download_file(
             client=client, 
@@ -47,11 +77,25 @@ async def download_with_progressbar(client, reply, msg):
             out=f,
             progress_callback=progress_bar
         )
-    await reply.edit("Finished downloading")
+    if end_message!=None:
+        await reply.edit(f"{end_message}")
     return download_location
 
-async def upload_with_progress_bar(client, reply, file_location, name=None, thumbnail=None):
+async def upload_with_progress_bar(client, file_location, reply=None, edit_message=None, end_message=None, name=None, thumbnail=None):
     timer = Timer()
+    if reply==None and edit_message==None:
+        raise  Nothing_Defined_Error
+    
+    if reply!=None and edit_message!=None:
+        raise Both_Defined_Error
+    ################## Checking if the argument given is valid or not ################
+    if reply==None:
+        reply=edit_message
+    try:
+        _=reply.id
+    except:
+        raise telethon.errors.rpcbaseerrors.NotFoundError
+    ###################################################################################
     if name == None:
         name = file_location.split("/")[-1]
     async def progress_bar(downloaded_bytes, total_bytes):
@@ -71,27 +115,37 @@ async def upload_with_progress_bar(client, reply, file_location, name=None, thum
         force_document=True,
         thumb=thumbnail
     )
-    await reply.edit("Finished uploading")
+    if end_message!=None:
+        await reply.edit(f"{end_message}")
     return the_message
 
-async def download_without_progressbar(client, msg):
+async def download_without_progressbar(client, msg, path, end_message=None):
     file = msg.document
     filename = msg.file.name
-    dir = f"downloads/"
+    dir = f"{path}"
+    try:
+        if os.path.exists(path):
+            pass
+        else:
+            os.makedirs(path)
+    except SyntaxError:
+        raise SyntaxError   
     if not filename:
         filename = (
             "video_" + dt.now().isoformat("_", "seconds") + ".mp4"
                     )
-    download_location = dir + filename
+    download_location =os.path.join(dir,filename)
     with open(download_location, "wb") as f:
         await download_file(
             client=client, 
             location=file, 
             out=f,
         )
+    if end_message!=None:
+        await msg.reply("")
     return download_location
 
-async def upload_without_progress_bar(client, entity, file_location, name=None, thumbnail=None):
+async def upload_without_progress_bar(client, entity, file_location, end_message=None, name=None, thumbnail=None):
     if name == None:
         name = file_location.split("/")[-1]
 
@@ -106,6 +160,8 @@ async def upload_without_progress_bar(client, entity, file_location, name=None, 
         force_document=True,
         thumb=thumbnail
     )
+    if end_message!=None:
+        await client.send_message(f"{end_message}")
     return the_message
 
 def progress_bar_str(done, total):
