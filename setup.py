b81c82bed1b67d46af0f24752c37da9a40ffb365
MiyukiKun <81800569+MiyukiKun@users.noreply.github.com>
2021-12-02 20:16:46 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     version="1.0.2",
     description="Make Telethon files upload/download faster",
     packages=find_packages(),
-    install_requires=["telethon", "cryptg", "pyaesni"],
+    install_requires=["telethon", "cryptg", "telethon-tgcrypto"],
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
